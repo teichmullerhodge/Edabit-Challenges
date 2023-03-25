cc2cd1d4f8cb6e92ff72851e98893fa7be15ce74
Matheus S <121572273+teichmullerhodge@users.noreply.github.com>
2023-03-25 14:28:35 -0300

    #![allow(non_snake_case)]
    #![allow(non_snake_case)]

---
+++ Hard/src/OddishvsEvenish.rs
@@ -0,0 +1,60 @@
+#![allow(non_snake_case)] /*Stupid warning. This is the most ridiculous warning i ever seen on a compiler. I'm going to add
+                          #![allow(non_snake_case)] to my rust files like i add <stdio.h> to a C file. */
+                            
+/*this program sums the digit of the number passed from command line and returns even or odd based on the sum.
+ 
+                            https://edabit.com/challenge/r6TSNwkLZ2DgsoKiH  */                           
+                            
+use std::env;
+
+fn main(){
+
+	let args: Vec<String> = env::args().collect(); //(int argc, char *argv[]) basically
+	let number = args[1].parse::<i32>().unwrap(); //args[1] is parsed from string to i32
+	OddishEven(number);
+
+
+
+
+}
+
+fn OddishEven(mut num: i32) -> i32 {
+
+	
+	let mut vec_ = Vec::new(); //vector to store all digits
+
+	while num > 0 {
+
+	let digit = num % 10;
+	vec_.push(digit);
+	num /= 10;
+
+	}
+
+	let sum: i32 = vec_.iter().sum(); //iterates over the elements in vec_ and add them to sum.
+
+	if sum % 2 == 1{ //case of sum is odd.
+
+		println!("Oddish!");
+		return 1;
+
+	}
+
+	if sum % 2 == 0{ //case of sum is even.
+
+		println!("Even!");
+		return 0;
+
+	}
+	
+	else{
+		println!("How in the name of god this happened?");
+		return -1; 
+		
+		/* For some reason YOUR function
+		has to have a return for all possibilities. 
+		BUT THERE'S NO POSSIBILITY OF YOU GETTING TO THIS POINT. */
+
+			}
+
+}
